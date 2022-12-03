50c2ed153a7e6383e3f2f2f073abd0a94b26ab5f
NNRepos <nivimano@gmail.com>
2022-12-03 07:15:54 +0200

    day 3

--- src/solutions/03.py
+++ src/solutions/03.py
@@ -1,17 +1,44 @@
 from src.utils.utils import *
 
 text = get_input(__file__)
-splittext = splitsplit(text)
 
 
 def part1():
-    print(text)
-    print(splittext)
-    return 0
+    total = 0
+    for line in text.splitlines():
+        ll = len(line) // 2
+        a, b = line[:ll], line[ll:]
+        sa = set(a)
+        sb = set(b)
+        for x in sa.intersection(sb):
+            if ord('a') <= ord(x) <= ord('z'):
+                total += (ord(x) - ord('a')) + 1
+            else:
+                total += (ord(x) - ord('A')) + 27
+
+    return total
 
 
 def part2():
-    return 0
+    total = 0
+    a, b, c = '', '', ''
+    for i, line in enumerate(text.splitlines()):
+        if i % 3 == 0:
+            a = line
+        elif i % 3 == 1:
+            b = line
+        else:
+            c = line
+            sa = set(a)
+            sb = set(b)
+            sc = set(c)
+            for x in sa.intersection(sb).intersection(sc):
+                if ord('a') <= ord(x) <= ord('z'):
+                    total += (ord(x) - ord('a')) + 1
+                else:
+                    total += (ord(x) - ord('A')) + 27
+
+    return total
 
 
 print("part1:", part1())
---
+++ src/solutions/04.py
@@ -0,0 +1,22 @@
+from src.utils.utils import *
+
+text = get_input(__file__)
+ss = splitsplit(text)
+
+
+def part1():
+    total = 0
+    print(text)
+    print(ss)
+    
+    return None
+
+
+def part2():
+    total = 0
+
+    return None
+
+
+print("part1:", part1())
+print("part2:", part2())
