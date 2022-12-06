270b4b3517f8a532d51404ffbcb7560efb236cf0
NNRepos <nivimano@gmail.com>
2022-12-06 07:11:50 +0200

    day 6

--- src/solutions/06.py
+++ src/solutions/06.py
@@ -5,16 +5,32 @@ ss = splitsplit(text, None)
 
 
 def part1():
-    ans = 0
-    print(text)
+    for i in range(3, len(text)):
+        a = text[i]
+        b = text[i - 1]
+        c = text[i - 2]
+        dd = text[i - 3]
+        if a == b or b == c or c == dd or a == c or a == dd or b == dd:
+            continue
+        return i + 1
 
-    return ans
+    return -1
 
 
 def part2():
-    ans = 0
+    for i in range(14, len(text)):
+        c = Counter(text[i - 14: i])
 
-    return ans
+        good = True
+        for k, v in c.items():
+            if v > 1:
+                good = False
+                break
+        if not good:
+            continue
+        return i
+
+    return -1
 
 
 print("part1:", part1())
---
+++ src/solutions/07.py
@@ -0,0 +1,18 @@
+from src.utils.utils import *
+
+text = get_input(__file__)
+ss = splitsplit(text, None)
+
+
+def part1():
+    print(text)
+
+    return 0
+
+
+def part2():
+    return 0
+
+
+print("part1:", part1())
+print("part2:", part2())
